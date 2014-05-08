b5fc28ae6e69b8a2ca6d29e20755810562080e99
ajinkya34 <ajinkya.godbole@outlook.com>
2014-05-08 01:51:59 -0700

    First Commit

---
+++ AzureTestProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureTestProject.Startup))]
+namespace AzureTestProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
